a58496d4d5c9f310ca4ecb8c1f7049c5bd588999
Nazar <nazarostap4uk@gmail.com>
2026-01-12 23:15:56 +0200

    configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,39 +1,22 @@
-import { defineConfig, devices } from ‘@playwright/test’;export default defineConfig({
+import { defineConfig, devices } from '@playwright/test'
 
-testDir: ‘./src/tests’,
-
-retries: 2,
-
-use: {
-
-headless: true,
-
-baseURL: ‘https://example.com’,
-
-viewport: { width: 1280, height: 720 },
-
-screenshot: ‘only-on-failure’,
-
-},
-
-projects: [
-
-{
-
-name: ‘Desktop Chromium’,
-
-use: { browserName: ‘chromium’ },
-
-},
-
-{
-
-name: ‘Mobile Safari’,
-
-use: { …devices[‘iPhone 12’] },
-
-},
-
-],
-
+export default defineConfig({
+  testDir: './src/tests',
+  retries: 2,
+  use: {
+	headless: true,
+	baseURL: 'https://www.ukr.net/',
+	viewport: { width: 1280, height: 720 },
+	screenshot: 'only-on-failure',
+  },
+  projects: [
+	{
+	  name: 'Desktop Chromium',
+	  use: { browserName: 'chromium' },
+	},
+	//{
+	//  name: 'Mobile Safari',
+	//  use: { ...devices['iPhone 12'] },
+	//},
+  ],
 });
